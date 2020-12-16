d24e7ef655186fb08fee3d088aa5dcd6eca485ee
Damien Stanton <damien.stanton@gmail.com>
2020-12-16 14:54:32 -0500

    still messing with testcase 27

--- data_structures/brackets/brackets.go
+++ data_structures/brackets/brackets.go
@@ -8,48 +8,79 @@ func main() {
 	fmt.Println(solve(str))
 }
 
-// handroll a stack
-type bytestack []byte
+// handroll a stack struct with the expected methods
+type runestack struct {
+	elements []rune
+	pos      int
+}
 
-func (s bytestack) pop() byte {
-	x := s[len(s)-1]
-	s = s[:len(s)-1]
+func newStackFrom(bb []rune) *runestack {
+	return &runestack{bb, 0}
+}
+
+func (s *runestack) push(b rune, i int) {
+	s.elements = append(s.elements, b)
+	s.pos = i
+}
+
+func (s *runestack) pop() rune {
+	n := len(s.elements)
+	x := s.elements[n-1]
+	s.elements = s.elements[:n-1]
 	return x
 }
 
-func (s bytestack) contains(b byte) bool {
-	for _, s := range s {
-		if s == b {
+func (s *runestack) contains(b rune) bool {
+	for _, e := range s.elements {
+		if b == e {
 			return true
 		}
 	}
 	return false
 }
 
+func pair(c rune) rune {
+	switch c {
+	case '}':
+		return '{'
+	case ']':
+		return '['
+	case ')':
+		return '('
+	default:
+		return rune(0)
+	}
+}
+
+// use the stack to balance parentheses
 func solve(str string) string {
-	opens := bytestack("{[(")
-	closes := bytestack("}])")
-	stack := make(bytestack, 0)
-	for i, c := range []byte(str) {
+	if len(str) <= 1 {
+		return "1"
+	}
+	opens := newStackFrom([]rune("{[("))
+	closes := newStackFrom([]rune("}])"))
+	stack := newStackFrom([]rune{})
+
+	for i, c := range str {
 		if opens.contains(c) {
-			stack = append(stack, c)
-		}
-		if len(stack) == 0 {
-			return fmt.Sprintf("%d", i)
-		}
-		if closes.contains(c) {
-			if string(c) == "}" && string(stack.pop()) != "{" {
-				return fmt.Sprintf("%d", i)
+			stack.push(c, i)
+		} else {
+			if closes.contains(c) {
+				if len(stack.elements) == 0 {
+					return fmt.Sprintf("%d", i+1)
+				}
+				want := pair(c)
+				top := stack.pop()
+				if top != want {
+					return fmt.Sprintf("%d", i+1)
+				}
 			}
-			if string(c) == "]" && string(stack.pop()) != "[" {
-				return fmt.Sprintf("%d", i)
-			}
-			if string(c) == ")" && string(stack.pop()) != "(" {
-				return fmt.Sprintf("%d", i)
-			}
-			stack.pop()
 		}
 	}
 
-	return "Success"
+	if len(stack.elements) == 0 {
+		return "Success"
+	}
+
+	return fmt.Sprintf("%d", stack.pos)
 }
